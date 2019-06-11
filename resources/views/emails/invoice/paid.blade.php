4451ce2b244eff8d7d29de7a21c3a79785213ce1
Xavier Pernalete <xavierp@xavierp-thi-m.local>
2019-06-11 16:24:34 -0600

    Add line blank on email

--- resources/views/emails/invoice/paid.blade.php
+++ resources/views/emails/invoice/paid.blade.php
@@ -1,8 +1,8 @@
 @component('mail::message')
-<p>Estimado Cliente: <b>{{$data_invoice->client_first_name .' '.$data_invoice->client_last_name}}</b><br>
+<p>Estimado Cliente: <b>{{$data_invoice->client_first_name .' '.$data_invoice->client_last_name}}</b><br><br>
     Adjunto a este correo encontrará un Comprobante Electrónico en formato XML y su correspondiente representación en
     formato PDF, por concepto de facturación de <b>{{$company->business_name}}</b>. Lo anterior con base en las
-    especificaciones del Ministerio de Hacienda. <br>
+    especificaciones del Ministerio de Hacienda. <br><br>
     ¡Muchas Gracias!
 </p>
 @endcomponent
