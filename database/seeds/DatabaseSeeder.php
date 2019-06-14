42a1c86da19e2964e17abefad99fbbcdbfa5edb4
Xavier Pernalete <xavierp@xavierp-thi-m.local>
2019-06-14 15:22:04 -0600

    Add seders on Databases Seeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(EtaxProductsSeeder::class);
+         $this->call(UnidadMedidaTableSeeder::class);
     }
 }
