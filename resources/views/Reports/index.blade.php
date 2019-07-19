6a4b376af71c85f49df82e08b578f2a76a8484e7
Ali Guillen <50844013+aguilleng@users.noreply.github.com>
2019-07-19 09:40:22 -0600

    Update index.blade.php

--- resources/views/Reports/index.blade.php
+++ resources/views/Reports/index.blade.php
@@ -10,7 +10,7 @@
 @endsection
 
 @section('content')
-
+<meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8">
 <div class="row">
   <div class="col-md-12">
     <div class="row">
@@ -82,7 +82,7 @@
         
       </div>
         <div class="col-md-1" hidden id="export_btn" style="margin-top:-2em;">
-            <button id="btnExport" onclick="javascript:xport.toXLS('reporte-container', 'reporte');" class="btn btn-primary form-btn">Descargar</button>
+            <button id="btnExport" onclick="Exportar();" class="btn btn-primary form-btn">Descargar</button>
         </div>
       
     </div>
@@ -103,104 +103,19 @@
     
   }
 
-  var xport = {
-      _fallbacktoCSV: true,
-      toXLS: function(tableId, filename) {
-          this._filename = (typeof filename == 'undefined') ? tableId : filename;
+ function Exportar(){
+      var uri = 'data:application/vnd.ms-excel;base64,'
+          , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head></head><body><table>{table}</table></body></html>'
+          , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+          , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
 
-          //var ieVersion = this._getMsieVersion();
-          //Fallback to CSV for IE & Edge
-          if ((this._getMsieVersion() || this._isFirefox()) && this._fallbacktoCSV) {
-              return this.toCSV(tableId);
-          } else if (this._getMsieVersion() || this._isFirefox()) {
-              alert("Not supported browser");
-          }
+      var table = 'reporte-container';
+      var name = 'nombre_hoja_calculo';
 
-          //Other Browser can download xls
-          var htmltable = document.getElementById(tableId);
-          var html = htmltable.outerHTML;
-
-          this._downloadAnchor("data:application/vnd.ms-excel" + encodeURIComponent(html), 'xls');
-      },
-      toCSV: function(tableId, filename) {
-          this._filename = (typeof filename === 'undefined') ? tableId : filename;
-          // Generate our CSV string from out HTML Table
-          var csv = this._tableToCSV(document.getElementById(tableId));
-          // Create a CSV Blob
-          var blob = new Blob([csv], { type: "text/csv" });
-
-          // Determine which approach to take for the download
-          if (navigator.msSaveOrOpenBlob) {
-              // Works for Internet Explorer and Microsoft Edge
-              navigator.msSaveOrOpenBlob(blob, this._filename + ".csv");
-          } else {
-              this._downloadAnchor(URL.createObjectURL(blob), 'csv');
-          }
-      },
-      _getMsieVersion: function() {
-          var ua = window.navigator.userAgent;
-
-          var msie = ua.indexOf("MSIE ");
-          if (msie > 0) {
-              // IE 10 or older => return version number
-              return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
-          }
-
-          var trident = ua.indexOf("Trident/");
-          if (trident > 0) {
-              // IE 11 => return version number
-              var rv = ua.indexOf("rv:");
-              return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
-          }
-
-          var edge = ua.indexOf("Edge/");
-          if (edge > 0) {
-              // Edge (IE 12+) => return version number
-              return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
-          }
-
-          // other browser
-          return false;
-      },
-      _isFirefox: function(){
-          if (navigator.userAgent.indexOf("Firefox") > 0) {
-              return 1;
-          }
-
-          return 0;
-      },
-      _downloadAnchor: function(content, ext) {
-          var anchor = document.createElement("a");
-          anchor.style = "display:none !important";
-          anchor.id = "downloadanchor";
-          document.body.appendChild(anchor);
-
-          // If the [download] attribute is supported, try to use it
-
-          if ("download" in anchor) {
-              anchor.download = this._filename + "." + ext;
-          }
-          anchor.href = content;
-          anchor.click();
-          anchor.remove();
-      },
-      _tableToCSV: function(table) {
-          // We'll be co-opting `slice` to create arrays
-          var slice = Array.prototype.slice;
-
-          return slice
-              .call(table.rows)
-              .map(function(row) {
-                  return slice
-                      .call(row.cells)
-                      .map(function(cell) {
-                          return '"t"'.replace("t", cell.textContent);
-                      })
-                      .join(",");
-              })
-              .join("\r\n");
-      }
-  };
+      if (!table.nodeType) table = document.getElementById(table)
+      var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML }
+      window.location.href = uri + base64(format(template, ctx))
+  }
   
   function verReporte() {
     var reporteView = $("#reportes-select").val();
