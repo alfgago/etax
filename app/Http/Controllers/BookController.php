ad936c5b4781e2079ab523f86df16a6490223c54
juanml19 <jmanueljf19@gmail.com>
2019-08-08 11:45:41 -0600

    año menor o igual al 2019

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -119,7 +119,7 @@ class BookController extends Controller
         })->get();
 
         $bloqueo = count($bills) + count($invoices);
-        if($book->year == 2019){
+        if($book->year <= 2019){
             if($book->month < 7){
                 $bloqueo = 0;
             }
