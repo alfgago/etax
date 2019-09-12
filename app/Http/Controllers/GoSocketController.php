4ee1dcbecf6842037ea7f0684deb6313fe8b637b
juanml19 <jmanueljf19@gmail.com>
2019-09-11 19:53:17 -0600

    agregar si no existe

--- app/Http/Controllers/GoSocketController.php
+++ app/Http/Controllers/GoSocketController.php
@@ -8,33 +8,21 @@ use GuzzleHttp\Exception\ClientException;
 use App\IntegracionEmpresa;
 use App\User;
 use App\Company;
+use App\Team;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Auth;
 
 class GoSocketController extends Controller
 {
      
     public function gosocketvalidate(Request $request) {
-    	$token = $request->token;
-    	if (!empty($token)) {
-            $ApplicationIdGS = config('etax.applicationidgs');
-            $base64 = base64_encode($ApplicationIdGS.":".$token);
-            $GoSocket = new Client();
-            $APIStatus = $GoSocket->request('GET', "http://api.sandbox.gosocket.net/api/Gadget/GetUser", [
-                'headers' => [
-                    'Content-Type' => "application/json",
-                    'Accept' => "application/json",
-                    'Authorization' => "Basic " . $base64
-                ],
-                'json' => [
-                ],
-                'verify' => false,
-            ]);
-            $user_gs = json_decode($APIStatus->getBody()->getContents(), true);
-            dd($user_gs);
-            $user = IntegracionEmpresa::where("access_token",$user_gs['UserId'])->where("company_token",$user_gs['CurrentAccountId'])->first();
-            if(is_null($user)){
+        try{
+        	$token = $request->token;
+        	if (!empty($token)) {
+                $ApplicationIdGS = config('etax.applicationidgs');
+                $base64 = base64_encode($ApplicationIdGS.":".$token);
                 $GoSocket = new Client();
-                $APIStatus = $GoSocket->request('GET', "http://api.sandbox.gosocket.net/api/Gadget/GetAccount?accountId=".$user_gs['CurrentAccountId'], [
+                $APIStatus = $GoSocket->request('GET', "http://api.sandbox.gosocket.net/api/Gadget/GetUser", [
                     'headers' => [
                         'Content-Type' => "application/json",
                         'Accept' => "application/json",
@@ -44,36 +32,94 @@ class GoSocketController extends Controller
                     ],
                     'verify' => false,
                 ]);
-                $company_gs = json_decode($APIStatus->getBody()->getContents(), true);
-                $user_etax = User::where('email',$user_gs['Email'])->first();
-                $company_etax = Company::where('id_number',$company_gs['Code'])->first();
+                $user_gs = json_decode($APIStatus->getBody()->getContents(), true);
+                
+                $user = IntegracionEmpresa::where("user_token",$user_gs['UserId'])->where("company_token",$user_gs['CurrentAccountId'])->first();
 
-                //dd($company_etax->team->id);
-                $new_user_gs = User::firstOrCreate(
-                    ['user_name' => "GS_".$user_gs['UserId']],
-                    ['email' => $user_etax['email'],
-                    'first_name' => $user_etax['first_name'],
-                    'last_name' => $user_etax['last_name'],
-                    'last_name2' => $user_etax['last_name2'],
-                    'phone' => $user_etax['phone'],
-                    'password' => 'password']
-                );
-                $E = "slug_" . $company_etax->id . "_" . $new_user_gs->id;
+                if(is_null($user)){
+                    $GoSocket = new Client();
+                    $APIStatus = $GoSocket->request('GET', "http://api.sandbox.gosocket.net/api/Gadget/GetAccount?accountId=".$user_gs['CurrentAccountId'], [
+                        'headers' => [
+                            'Content-Type' => "application/json",
+                            'Accept' => "application/json",
+                            'Authorization' => "Basic " . $base64
+                        ],
+                        'json' => [
+                        ],
+                        'verify' => false,
+                    ]);
+                    $company_gs = json_decode($APIStatus->getBody()->getContents(), true);
+                    $user_etax = User::firstOrCreate(
+                        ['email' => $user_gs['Email']],
+                        ['user_name' => $user_gs['Email'],
+                        'first_name' => $user_gs['Name'],
+                        'password' => 'asd889as56ad9as66asd6as9']
+                    );
+
+                    $new_user_gs = User::firstOrCreate(
+                        ['user_name' => "GS_".$user_gs['UserId']],
+                        ['email' => $user_etax['email'].".gs",
+                        'first_name' => $user_etax['first_name'],
+                        'last_name' => $user_etax['last_name'],
+                        'last_name2' => $user_etax['last_name2'],
+                        'phone' => $user_etax['phone'],
+                        'password' => 'password']
+                    );
 
 
-            }
-            // dd("asdas");
-            if ($user !== null && Auth::loginUsingId($user->user_id)) {
-                /*	$user = auth()->user();
-                    $companyId = $request->companyId;
-                    $team = Team::where( 'company_id', $companyId )->first();
-                    $user->switchTeam( $team );
-                    Cache::forget("cache-currentcompany-$user->id");*/
-                return redirect('/');
+                    $company_etax = Company::firstOrCreate(
+                        ['id_number' => $company_gs['Code']],
+                        ['user_id' => $user_etax['id'],
+                        'business_name' => $company_gs['Name'],
+                        'id_number' => $company_gs['Code'],
+                        'district' => $company_gs['Address'],
+                        'city' => $company_gs['City'],
+                        'state' => $company_gs['Province'],
+                        'email' => $company_gs['ContactEmail']
+                        ]
+                    );
+                    $team = Team::firstOrCreate(
+                        [
+                            'owner_id' => $user_etax->id,
+                            'company_id' => $company_etax->id
+                        ],
+                        [    'name' => "slug_" . $company_etax->id . "_" . $user_etax->id,
+                            'slug' => "(" . $company_etax->id . ") -" .  $user_etax->id
+                        ]
+                    );
+
+                    $team->save();
+                    $user_etax->attachTeam($team);
+                    $new_user_gs->attachTeam($team);
+                    $user = IntegracionEmpresa::Create(
+                        [  "user_token"=> $user_gs['UserId'],
+                            "company_token"=> $user_gs['CurrentAccountId'],
+                            "integration_id"=> 1,
+                           "user_id"=> $new_user_gs->id,
+                            "company_id"=> $company_etax->id,
+                            "status"=> 1
+                        ]
+                    );
+                }
+                if ($user !== null && Auth::loginUsingId($user->user_id)) {
+                        $user->session_token = $token;
+                        $user->save();
+
+                        $companyId = $user->company_id;
+                    	$user_login = auth()->user();
+                        $team = Team::where( 'company_id', $companyId )->first();
+                        $user_login->switchTeam( $team );
+                        Cache::forget("cache-currentcompany-$user_login->id");
+                    return redirect('/');
+                } else {
+                    return redirect('/login');
+                }
             } else {
                 return redirect('/login');
             }
-        } else {
+        }catch( \Exception $ex ){
+            return redirect('/login');
+        }catch( \Throwable $ex ){
             return redirect('/login');
         }
 	    
--- app/IntegracionEmpresa.php
+++ app/IntegracionEmpresa.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class IntegracionEmpresa extends Model
 {
-    //
+     protected $fillable = [
+        'user_token',
+        "company_token",
+        "integration_id",
+        "user_id",
+        "company_id",
+        "status"
+    ];
 }
