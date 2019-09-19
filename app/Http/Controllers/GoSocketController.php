c1dd9fb823caa965f8a62bc2f899512204a77b75
juanml19 <jmanueljf19@gmail.com>
2019-09-19 16:41:18 -0600

    sincronizacion compra y venta

--- app/Http/Controllers/GoSocketController.php
+++ app/Http/Controllers/GoSocketController.php
@@ -9,6 +9,8 @@ use App\IntegracionEmpresa;
 use App\User;
 use App\Company;
 use App\Team;
+use App\Invoice;
+use App\Bill;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
@@ -17,7 +19,7 @@ class GoSocketController extends Controller
 {
      
     public function gosocketValidate(Request $request) {
-        try{
+        //try{
         	$token = $request->token;
         	if (!empty($token)) {
                 $ApplicationIdGS = config('etax.applicationidgs');
@@ -104,50 +106,128 @@ class GoSocketController extends Controller
                 if ($user !== null && Auth::loginUsingId($user->user_id)) {
                         $user->session_token = $token;
                         $user->save();
-
                         $companyId = $user->company_id;
                     	$user_login = auth()->user();
                         $team = Team::where( 'company_id', $companyId )->first();
                         $user_login->switchTeam( $team );
+                        
                         Cache::forget("cache-currentcompany-$user_login->id");
-                    return redirect('/');
+                        $this->getInvoices($user);
+                        $this->getBills($user);
+                        dd($user);
+                    //return redirect('/');
                 } else {
                     return redirect('/login');
                 }
             } else {
                 return redirect('/login');
             }
-        }catch( \Exception $ex ) {
+       /* }catch( \Exception $ex ) {
             Log::error("Error en login gosocket $ex");
             return redirect('/login');
         }catch( \Throwable $ex ) {
             Log::error("Error en login gosocket $ex");
             return redirect('/login');
-        }
+        }*/
 	    
     }
      
     
-    public function getInvoices(){
-    	$token = $_GET['token'];
+    public function getInvoices($user){
+        $token = $user->session_token;
     	$ApplicationIdGS = config('etax.applicationidgs');
 		$base64 = base64_encode($ApplicationIdGS.":".$token);
     	$GoSocket = new Client();
-	    $APIStatus = $GoSocket->request('GET', "http://api.sandbox.gosocket.net/api/Gadget/GetUser", [
+	    $APIStatus = $GoSocket->request('GET', "http://api.sandbox.gosocket.net/api/Gadget/GetSentDocuments?MyAccountId=fbcd6a77-4396-49d3-85cb-8646453e8460&fromDate=2019-01-01&toDate=2020-01-01&DocumentTypeId=1&ReceiverCode=-1&Number=-1&Page=1&ReadMode=json ", [
 	        'headers' => [
 	            'Content-Type' => "application/json",
 	            'Accept' => "application/json", 
 	            'Authorization' => "Basic " . $base64
 	        ],
-	        'json' => [
-	        ],
+	        'json' => [],
 	        'verify' => false,
 	    ]);
-	    $APIStatus = json_decode($APIStatus->getBody()->getContents(), true);
-	    dd($APIStatus);
-	    
+	    $facturas = json_decode($APIStatus->getBody()->getContents(), true);
+        //dd($facturas);
+        foreach ($facturas as $factura) {
+            $GoSocket = new Client();
+            $APIStatus = $GoSocket->request('GET', "http://api.sandbox.gosocket.net/api/Gadget/GetXml?DocumentId=".$factura['DocumentId']."", [
+                'headers' => [
+                    'Content-Type' => "application/json",
+                    'Accept' => "application/json", 
+                    'Authorization' => "Basic " . $base64
+                ],
+                'json' => [],
+                'verify' => false,
+            ]);
+            $APIStatus = json_decode($APIStatus->getBody()->getContents(), true);
+
+            $company = currentCompanyModel();
+            $xml  = base64_decode($APIStatus);
+            $xml = simplexml_load_string( $xml);
+            $json = json_encode( $xml );
+            $arr = json_decode( $json, TRUE );
+            try { 
+                $identificacionReceptor = array_key_exists('Receptor', $arr) ? $arr['Receptor']['Identificacion']['Numero'] : 0 ;
+            }catch(\Exception $e){ $identificacionReceptor = 0; };
+                        
+            $identificacionEmisor = $arr['Emisor']['Identificacion']['Numero'];
+            $consecutivoComprobante = $arr['NumeroConsecutivo'];
+                    
+            //Compara la cedula de Receptor con la cedula de la compañia actual. Tiene que ser igual para poder subirla
+            if( preg_replace("/[^0-9]+/", "", $company->id_number) == preg_replace("/[^0-9]+/", "", $identificacionEmisor ) ) {
+                //Registra el XML. Si todo sale bien, lo guarda en S3.
+                $invoice = Invoice::saveInvoiceXML( $arr, 'GS' );
+            }
+            $company->save();
+        }  
     }
 
 
+    public function getBills($user){
+        $token = $user->session_token;
+        $ApplicationIdGS = config('etax.applicationidgs');
+        $base64 = base64_encode($ApplicationIdGS.":".$token);
+        $GoSocket = new Client();
+        $APIStatus = $GoSocket->request('GET', "http://api.sandbox.gosocket.net/api/Gadget/GetReceivedDocuments?MyAccountId=fbcd6a77-4396-49d3-85cb-8646453e8460&fromDate=2019-01-01&toDate=2020-01-01&DocumentTypeId=1&ReceiverCode=-1&Number=-1&Page=1&ReadMode=json ", [
+            'headers' => [
+                'Content-Type' => "application/json",
+                'Accept' => "application/json", 
+                'Authorization' => "Basic " . $base64
+            ],
+            'json' => [],
+            'verify' => false,
+        ]);
+        $facturas = json_decode($APIStatus->getBody()->getContents(), true);
+        //dd($facturas);
+        foreach ($facturas as $factura) {
+            $GoSocket = new Client();
+            $APIStatus = $GoSocket->request('GET', "http://api.sandbox.gosocket.net/api/Gadget/GetXml?DocumentId=".$factura['DocumentId']."", [
+                'headers' => [
+                    'Content-Type' => "application/json",
+                    'Accept' => "application/json", 
+                    'Authorization' => "Basic " . $base64
+                ],
+                'json' => [],
+                'verify' => false,
+            ]);
+            $APIStatus = json_decode($APIStatus->getBody()->getContents(), true);
+            $company = currentCompanyModel();
+            $xml  = base64_decode($APIStatus);
+            $xml = simplexml_load_string( $xml);
+            $json = json_encode( $xml );
+            $arr = json_decode( $json, TRUE );
+            $identificacionReceptor = array_key_exists('Receptor', $arr) ? $arr['Receptor']['Identificacion']['Numero'] : 0;
+            $identificacionEmisor = $arr['Emisor']['Identificacion']['Numero'];
+            $consecutivoComprobante = $arr['NumeroConsecutivo'];
+            $clave = $arr['Clave'];
+            //Compara la cedula de Receptor con la cedula de la compañia actual. Tiene que ser igual para poder subirla
+            if( preg_replace("/[^0-9]+/", "", $company->id_number) == preg_replace("/[^0-9]+/", "", $identificacionReceptor ) ) {
+                //Registra el XML. Si todo sale bien, lo guarda en S3
+                $bill = Bill::saveBillXML( $arr, 'XML' );
+            }
+            $company->save();
+        }  
+    }
 
 }
