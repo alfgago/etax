774c5c0cfad22b6a78cf86b638975633d91680c0
juanml19 <jmanueljf19@gmail.com>
2019-10-02 11:48:20 -0600

    cambios

--- app/Jobs/GoSocketInvoicesSync.php
+++ app/Jobs/GoSocketInvoicesSync.php
@@ -39,30 +39,32 @@ class GoSocketInvoicesSync implements ShouldQueue
     private function getInvoices($user) {
         $token = $user->session_token;
         $apiGoSocket = new BridgeGoSocketApi();
-        $facturas = $apiGoSocket->getSentDocuments($token, $user->company_token);
+        $tipos_facturas = $apiGoSocket->getDocumentTypes($token);
+        foreach ($tipos_facturas as $tipo_factura) {
+            $facturas = $apiGoSocket->getSentDocuments($token, $user->company_token, $tipo_factura);
+            foreach ($facturas as $factura) {
+                $APIStatus = $apiGoSocket->getXML($token, $factura['DocumentId']);
+                $company = currentCompanyModel();
+                $xml  = base64_decode($APIStatus);
+                $xml = simplexml_load_string( $xml);
+                $json = json_encode( $xml );
+                $arr = json_decode( $json, TRUE );
+                try {
+                    $identificacionReceptor = array_key_exists('Receptor', $arr) ? $arr['Receptor']['Identificacion']['Numero'] : 0 ;
+                } catch(\Exception $e) {
+                    $identificacionReceptor = 0;
+                };
 
-        foreach ($facturas as $factura) {
-            $APIStatus = $apiGoSocket->getXML($token, $factura['DocumentId']);
-            $company = currentCompanyModel();
-            $xml  = base64_decode($APIStatus);
-            $xml = simplexml_load_string( $xml);
-            $json = json_encode( $xml );
-            $arr = json_decode( $json, TRUE );
-            try {
-                $identificacionReceptor = array_key_exists('Receptor', $arr) ? $arr['Receptor']['Identificacion']['Numero'] : 0 ;
-            } catch(\Exception $e) {
-                $identificacionReceptor = 0;
-            };
+                $identificacionEmisor = $arr['Emisor']['Identificacion']['Numero'];
+                $consecutivoComprobante = $arr['NumeroConsecutivo'];
 
-            $identificacionEmisor = $arr['Emisor']['Identificacion']['Numero'];
-            $consecutivoComprobante = $arr['NumeroConsecutivo'];
-
-            //Compara la cedula de Receptor con la cedula de la compañia actual. Tiene que ser igual para poder subirla
-            if( preg_replace("/[^0-9]+/", "", $company->id_number) == preg_replace("/[^0-9]+/", "", $identificacionEmisor ) ) {
-                //Registra el XML. Si todo sale bien, lo guarda en S3.
-                Invoice::saveInvoiceXML( $arr, 'GS' );
+                //Compara la cedula de Receptor con la cedula de la compañia actual. Tiene que ser igual para poder subirla
+                if( preg_replace("/[^0-9]+/", "", $company->id_number) == preg_replace("/[^0-9]+/", "", $identificacionEmisor ) ) {
+                    //Registra el XML. Si todo sale bien, lo guarda en S3.
+                    Invoice::saveInvoiceXML( $arr, 'GS' );
+                }
+                $company->save();
             }
-            $company->save();
         }
     }
 
