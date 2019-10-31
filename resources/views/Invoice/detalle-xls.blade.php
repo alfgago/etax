b6d60b089a6faaf00b489d779ef94c6841faa7ed
juanml19 <jmanueljf19@gmail.com>
2019-10-31 16:37:19 -0600

    Update detalle-xls.blade.php

--- resources/views/Invoice/detalle-xls.blade.php
+++ resources/views/Invoice/detalle-xls.blade.php
@@ -1,8 +1,8 @@
 <div class="row">
 	<div class="col-md-3 col-sm-12">
 		<h3>Cliente</h3><br>
-		<label>nombreReceptor</label>: {{$factura[0]->nombreReceptor}}<br>
-		<label>tipoIdentificacion</label>: {{$factura[0]->tipoIdentificacionReceptor}}<br>
+		<label>nombre Receptor</label>: {{$factura[0]->nombreReceptor}}<br>
+		<label>tipo Identificacion</label>: {{$factura[0]->tipoIdentificacionReceptor}}<br>
 		<label>Identificacion</label>: {{$factura[0]->IdentificacionReceptor}}<br>
 		<label>correo</label>: {{$factura[0]->correoReceptor}}<br>
 		<!--
@@ -13,29 +13,29 @@
 	</div>
 	<div class="col-md-3 col-sm-12">
 		<h3>Detalle Factura</h3><br>
-		<label>condicionVenta</label>: {{$factura[0]->condicionVenta}}<br>
-		<label>plazoCredito</label>: {{$factura[0]->plazoCredito}}<br>
-		<label>medioPago</label>: {{$factura[0]->medioPago}}<br>
-		<label>codigoMoneda</label>: {{$factura[0]->codigoMoneda}}<br>
-		<label>tipoCambio</label>: {{$factura[0]->tipoCambio}}<br>
+		<label>condicion Venta</label>: {{$factura[0]->condicionVenta}}<br>
+		<label>plazo Credito</label>: {{$factura[0]->plazoCredito}}<br>
+		<label>medio Pago</label>: {{$factura[0]->medioPago}}<br>
+		<label>codigo Moneda</label>: {{$factura[0]->codigoMoneda}}<br>
+		<label>tipo Cambio</label>: {{$factura[0]->tipoCambio}}<br>
 	</div>
 	<div class="col-md-3 col-sm-12">
 		<h3>Totales Detallados</h3><br>
-		<label>totalServGravados</label>: {{$factura[0]->totalServGravados}}<br>
-		<label>totalServExentos</label>: {{$factura[0]->totalServExentos}}<br>
-		<label>totalMercanciasGravadas</label>: {{$factura[0]->totalMercanciasGravadas}}<br>
-		<label>totalMercanciasExentas</label>: {{$factura[0]->totalMercanciasExentas}}<br>
-		<label>totalGravado</label>: {{$factura[0]->totalGravado}}<br>
-		<label>totalExento</label>: {{$factura[0]->totalExento}}<br>
+		<label>total Servicios Gravados</label>: {{$factura[0]->totalServGravados}}<br>
+		<label>total Servicios Exentos</label>: {{$factura[0]->totalServExentos}}<br>
+		<label>total Mercancias Gravadas</label>: {{$factura[0]->totalMercanciasGravadas}}<br>
+		<label>total Mercancias Exentas</label>: {{$factura[0]->totalMercanciasExentas}}<br>
+		<label>total Gravado</label>: {{$factura[0]->totalGravado}}<br>
+		<label>total Exento</label>: {{$factura[0]->totalExento}}<br>
 	</div>
 	<div class="col-md-3 col-sm-12">
 		<h3>Totales Factura</h3><br>
-		<label>totalVenta</label>: {{$factura[0]->totalVenta}}<br>
-		<label>totalDescuentos</label>: {{$factura[0]->totalDescuentos}}<br>
-		<label>totalVentaNeta</label>: {{$factura[0]->totalVentaNeta}}<br>
-		<label>totalImpuesto</label>: {{$factura[0]->totalImpuesto}}<br>
-		<label>totalOtrosCargos</label>: {{$factura[0]->totalOtrosCargos}}<br>
-		<label>totalComprobante</label>: {{$factura[0]->totalComprobante}}<br>
+		<label>total Venta</label>: {{$factura[0]->totalVenta}}<br>
+		<label>total Descuentos</label>: {{$factura[0]->totalDescuentos}}<br>
+		<label>total Venta Neta</label>: {{$factura[0]->totalVentaNeta}}<br>
+		<label>total Impuesto</label>: {{$factura[0]->totalImpuesto}}<br>
+		<label>total Otros Cargos</label>: {{$factura[0]->totalOtrosCargos}}<br>
+		<label>total Comprobante</label>: {{$factura[0]->totalComprobante}}<br>
 	</div>
 </div>
 <div class="row">
@@ -44,19 +44,19 @@
 		<table id="invoice-table" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
           <thead>
             <tr>
-            	<th>numeroLinea</th>
+            	<th>numero Linea</th>
 				<th>cantidad</th>
-				<th>unidadMedida</th>
+				<th>unidad Medida</th>
 				<th>detalle</th>
-				<th>precioUnitario</th>
-				<th>montoTotal</th>
-				<th>montoDescuento</th>
-				<th>naturalezaDescuento</th>
+				<th>precio Unitario</th>
+				<th>monto Total</th>
+				<th>monto Descuento</th>
+				<th>naturaleza Descuento</th>
 				<th>subTotal</th>
-				<th>codigoImpuesto</th>
-				<th>codigoTarifa</th>
-				<th>tarifaImpuesto</th>
-				<th>montoImpuesto</th>
+				<th>codigo Impuesto</th>
+				<th>codigo Tarifa</th>
+				<th>tarifa Impuesto</th>
+				<th>monto Impuesto</th>
 				<!--
 				<th>tipoDocumentoExoneracion</th> 
 				<th>numeroDocumentoExoneracion</th> 
@@ -64,34 +64,36 @@
 				<th>fechaEmisionExoneracion</th> 
 				<th>porcentajeExoneracionExoneracion</th> 
 				<th>montoExoneracionExoneracion</th> -->
-				<th>montoTotalLinea</th> 
+				<th>monto Total Linea</th> 
             </tr>
           </thead>
           <tbody>
 			@foreach ( $factura as $linea )
-	            <tr>
-	            	<td>{{$linea->numeroLinea}}</td>
-					<td>{{$linea->cantidad}}</td>
-					<td>{{$linea->unidadMedida}}</td>
-					<td>{{$linea->detalle}}</td>
-					<td>{{$linea->precioUnitario}}</td>
-					<td>{{$linea->montoTotal}}</td>
-					<td>{{$linea->montoDescuento}}</td>
-					<td>{{$linea->naturalezaDescuento}}</td>
-					<td>{{$linea->subTotal}}</td>
-					<td>{{$linea->codigoImpuesto}}</td>
-					<td>{{$linea->codigoTarifa}}</td>
-					<td>{{$linea->tarifaImpuesto}}</td>
-					<td>{{$linea->montoImpuesto}}</td>
-					<!--
-					<td>{{$linea->tipoDocumentoExoneracion}}</td> 
-					<td>{{$linea->numeroDocumentoExoneracion}}</td> 
-					<td>{{$linea->nombreInstitucionExoneracion}}</td> 
-					<td>{{$linea->fechaEmisionExoneracion}}</td> 
-					<td>{{$linea->porcentajeExoneracionExoneracion}}</td> 
-					<td>{{$linea->montoExoneracionExoneracion}}</td> -->
-					<td>{{$linea->montoTotalLinea}}</td> 
-	            </tr>
+				@if($linea->tipoLinea == 1)
+		            <tr>
+		            	<td>{{$linea->numeroLinea}}</td>
+						<td>{{$linea->cantidad}}</td>
+						<td>{{$linea->unidadMedida}}</td>
+						<td>{{$linea->detalle}}</td>
+						<td>{{$linea->precioUnitario}}</td>
+						<td>{{$linea->montoTotal}}</td>
+						<td>{{$linea->montoDescuento}}</td>
+						<td>{{$linea->naturalezaDescuento}}</td>
+						<td>{{$linea->subTotal}}</td>
+						<td>{{$linea->codigoImpuesto}}</td>
+						<td>{{$linea->codigoTarifa}}</td>
+						<td>{{$linea->tarifaImpuesto}}</td>
+						<td>{{$linea->montoImpuesto}}</td>
+						<!--
+						<td>{{$linea->tipoDocumentoExoneracion}}</td> 
+						<td>{{$linea->numeroDocumentoExoneracion}}</td> 
+						<td>{{$linea->nombreInstitucionExoneracion}}</td> 
+						<td>{{$linea->fechaEmisionExoneracion}}</td> 
+						<td>{{$linea->porcentajeExoneracionExoneracion}}</td> 
+						<td>{{$linea->montoExoneracionExoneracion}}</td> -->
+						<td>{{$linea->montoTotalLinea}}</td> 
+		            </tr>
+	            @endif
 			@endforeach
            </tbody>
         </table>
