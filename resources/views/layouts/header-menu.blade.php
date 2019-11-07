eb4f4941efa63e3d3ff2ba8c46c48eafcdc32cd4
juanml19 <jmanueljf19@gmail.com>
2019-11-07 14:53:23 -0600

    Update header-menu.blade.php

--- resources/views/layouts/header-menu.blade.php
+++ resources/views/layouts/header-menu.blade.php
@@ -55,10 +55,12 @@
                         <a class="dropdown-item" {{$item->type}}="{{$item->link}}">{{$item->name}}</a>
                     <?php } ?>
 
+                    <!-- @if( !in_array(8, auth()->user()->permisos()))     -->
                         <a class="dropdown-item" onclick="event.preventDefault(); document.getElementById(&quot;frm-logout&quot;).submit();">Cerrar sesión</a>
                     <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
                         {{ csrf_field() }} 
                     </form>
+                    <!-- @endif -->
                 </div>
             </div>
         </div>
