f984926ee753ea6cab016396864d480d98df12e2
juanml19 <jmanueljf19@gmail.com>
2019-11-15 17:29:34 -0600

    Update configuracion.blade.php

--- resources/views/gosocket/configuracion.blade.php
+++ resources/views/gosocket/configuracion.blade.php
@@ -1,8 +1,23 @@
-@extends('layouts/gosocket') 
+@extends('layouts/wizard-layout')
 
-@section('title') GoSocket @endsection 
+@section('title') 
+  Configuración inicial
+@endsection
 
-@section('content')
+@section('slug', 'wizard')
+
+@section('header-scripts')
+
+@endsection
+
+@section('content') 
+<div class="wizard-container">
+  <div class="wizard-popup" id="wizard-popup">
+  	
+   	<div class="titulo-bienvenida">
+    	<h2>Configuración inicial de empresa</h2>
+    	<p>Para iniciar, complete sus datos a continuación.</p>
+    </div>
 <div class="form-group col-md-12">
     <label for="tipo_persona">Actividades comerciales *</label>
     <select class="form-control checkEmpty select2-tags" name="commercial_activities[]" id="commercial_activities" multiple required>
@@ -60,5 +75,98 @@
   <button type="button" class="btn btn-primary btn-prev" onclick="toggleStep('step3');">Paso anterior</button>
   <button type="submit" id="btn-submit" class="btn btn-primary btn-next" onclick="trackClickEvent( 'TerminarConfigInicial' );">Terminar configuración inicial</button>
 </div>
+@endsection
+
+
+
+@section('footer-scripts')
+
+<script>
+	
+	function toggleStep(id) {
+	    var fromId = $('.step-btn.is-active').attr('id');
+	    var allow = checkEmptyFields(fromId);
+	    
+	    if( allow )	{
+    		$('.step-section, .step-btn').removeClass('is-active');
+    		$('.'+id).addClass('is-active');
+    		$('.wizard-container').prop('class', id+'-selected wizard-container');
+	    }
+	}
+
+    function checkEmptyFields(id) {
+      var allow = true;
+      $('.'+id+' .checkEmpty').each( function() {
+        	
+    		if( $(this).val() && $(this).val() != "" ) {
+    		    $(this).removeClass('isEmptyRequired');
+    		    $(this).parent().find('label').attr('style', '');
+    		}
+    		else {
+    		    $(this).addClass('isEmptyRequired');
+    		    allow = false;
+    		    $(this).parent().find('label').attr('style', 'color:red;');
+    		}
+    		
+    		if(allow) {
+	    		//Revisa que el campo de correo este correcto
+	    		var email = $('#email').val();
+	    		allow = validateEmail(email);
+    		}
+    		
+    	});
+    	return allow;
+    }
+    
+    function validateEmail(email) {
+		  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		  return re.test(email);
+		}
+    
+    function toggleTipoProrrata() {
+	  var metodo = $("#first_prorrata_type").val();
+		  $( ".toggle-types" ).hide();
+		  $( ".type-"+metodo ).show();
+		}
+    
+    $( document ).ready(function() {  
+        fillProvincias();
+        toggleTipoProrrata();
+        toggleApellidos();
+    });
+
+	$("#input-cert").change(function () {
+		var ext = this.value.match(/\.(.+)$/)[1];
+		switch (ext) {
+			case 'p12':
+				$('#uploadButton').attr('disabled', false);
+				break;
+			default:
+				alert('El archivo no es un certificado.');
+				this.value = '';
+		}
+	});
+
+	$("#input_logo").change(function () {
+		var ext = this.value.match(/\.(.+)$/)[1];
+		switch (ext) {
+			case 'jpg':
+			case 'jpeg':
+			case 'png':
+				$('#uploadButton').attr('disabled', false);
+				break;
+			default:
+				alert('El archivo no es de tipo imagen.');
+				this.value = '';
+		}
+
+		var file_size = $("#input_logo")[0].files[0].size;
+		if(file_size > 2097152) {
+			alert('El el archivo debe tener un maximo de 2MB.');
+			$("#input_logo").value = '';
+		}
+	});
+
+</script>
 
 @endsection
