37f5db2db9c71e56a3e5a78f47b939f7f869e74b
juanml19 <jmanueljf19@gmail.com>
2019-11-20 11:01:18 -0600

    notificacion falla

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -748,7 +748,7 @@ class BillController extends Controller
     }
     
     public function importXML(Request $request) {
-        //try {
+        try {
             $time_start = getMicrotime();
             $company = currentCompanyModel();
             $file = Input::file('file');
@@ -785,10 +785,10 @@ class BillController extends Controller
                             ->onQueue('log_queue');
                         }
                     }else{
-                        return Response()->json("El documento $consecutivoComprobante no le pertenece a su empresa actual", 400);
+                        return Response()->json("El documento no le pertenece a su empresa actual.", 400);
                     }
                 }else{
-                    return Response()->json('Error: El mes de la factura ya fue cerrado', 400);
+                    return Response()->json('Error: El mes de la factura ya fue cerrado.', 400);
                 }
             
 
@@ -796,17 +796,17 @@ class BillController extends Controller
             $company->save();
             $time_end = getMicrotime();
             $time = $time_end - $time_start;
-        /*}catch( \Exception $ex ){
+        }catch( \Exception $ex ){
             Log::error('Error importando XML ' . $ex->getMessage());
             return Response()->json('Se ha detectado un error en el tipo de archivo subido.', 400);
         }catch( \Throwable $ex ){
             Log::error('Error importando XML ' . $ex->getMessage());
             return Response()->json('Se ha detectado un error en el tipo de archivo subido.', 400);
-        }*/
-        if( substr($arr['NumeroConsecutivo'],8,2) == "04" ) {
-            return Response()->json('Se importo un tiquete sin identificación.', 206 );
         }
-        return Response()->json('Facturas importados exitosamente en '.$time.'s', 200);
+        if( substr($arr['NumeroConsecutivo'],8,2) == "04" ) {
+            return Response()->json('Se importo un tiquete sin cedula de receptor.', 206 );
+        }
+        return Response()->json('Factura importada exitosamente.', 200);
 
     }
     
