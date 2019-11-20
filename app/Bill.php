785e90b7c77e46c7a6ceb437cc8a69c8612d0f92
juanml19 <jmanueljf19@gmail.com>
2019-11-19 19:33:26 -0600

    cargar

--- app/Bill.php
+++ app/Bill.php
@@ -319,7 +319,7 @@ class Bill extends Model
     
     public static function saveBillXML( $arr, $metodoGeneracion ) {
         
-        $identificacionReceptor = $arr['Receptor']['Identificacion']['Numero'];
+        $identificacionReceptor = array_key_exists('Receptor', $arr) ? $arr['Receptor']['Identificacion']['Numero'] : 0;
         if($metodoGeneracion != "Email" && $metodoGeneracion != 'GS' ){
           $company = currentCompanyModel();
         }else{
--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -748,26 +748,24 @@ class BillController extends Controller
     }
     
     public function importXML(Request $request) {
-        try {
+        //try {
             $time_start = getMicrotime();
             $company = currentCompanyModel();
             $file = Input::file('file');
             $xml = simplexml_load_string( file_get_contents($file) );
             $json = json_encode( $xml ); // convert the XML string to JSON
-            $arr = json_decode( $json, TRUE );                
-            //if(substr($arr['NumeroConsecutivo'],8,2) != "04"){
+            $arr = json_decode( $json, TRUE ); 
                 $FechaEmision = explode("T", $arr['FechaEmision']);
                 $FechaEmision = explode("-", $FechaEmision[0]);
                 $FechaEmision = $FechaEmision[2]."/".$FechaEmision[1]."/".$FechaEmision[0];
 
                 if(CalculatedTax::validarMes($FechaEmision)){
                     $identificacionReceptor = array_key_exists('Receptor', $arr) ? $arr['Receptor']['Identificacion']['Numero'] : 0;
-                    $identificacionEmisor = $arr['Emisor']['Identificacion']['Numero'];
+                    $identificacionEmisor = array_key_exists('Emisor', $arr) ? $arr['Emisor']['Identificacion']['Numero'] : 0;
                     $consecutivoComprobante = $arr['NumeroConsecutivo'];
                     $clave = $arr['Clave'];
-                    
                     //Compara la cedula de Receptor con la cedula de la compañia actual. Tiene que ser igual para poder subirla
-                    if( preg_replace("/[^0-9]+/", "", $company->id_number) == preg_replace("/[^0-9]+/", "", $identificacionReceptor ) ) {
+                    if( preg_replace("/[^0-9]+/", "", $company->id_number) == preg_replace("/[^0-9]+/", "", $identificacionReceptor )  || substr($arr['NumeroConsecutivo'],8,2) == "04" ) {
                         //Registra el XML. Si todo sale bien, lo guarda en S3
                         $bill = Bill::saveBillXML( $arr, 'XML' );
                         if( $bill ) {
@@ -792,22 +790,22 @@ class BillController extends Controller
                 }else{
                     return Response()->json('Error: El mes de la factura ya fue cerrado', 400);
                 }
-            /*}else{
-                return Response()->json('Error: No se puede importar un tiquete electrónico.', 400);
-            }*/
+            
 
              
             $company->save();
             $time_end = getMicrotime();
             $time = $time_end - $time_start;
-        }catch( \Exception $ex ){
+        /*}catch( \Exception $ex ){
             Log::error('Error importando XML ' . $ex->getMessage());
             return Response()->json('Se ha detectado un error en el tipo de archivo subido.', 400);
         }catch( \Throwable $ex ){
             Log::error('Error importando XML ' . $ex->getMessage());
             return Response()->json('Se ha detectado un error en el tipo de archivo subido.', 400);
+        }*/
+        if( substr($arr['NumeroConsecutivo'],8,2) == "04" ) {
+            return Response()->json('Se importo un tiquete sin identificación.', 206 );
         }
-
         return Response()->json('Facturas importados exitosamente en '.$time.'s', 200);
 
     }
