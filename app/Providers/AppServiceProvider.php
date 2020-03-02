c9b3c8de3878c800c6f36f1b5eb6481c5aef8955
Alfredo Gago <alfgago@gmail.com>
2020-03-02 07:08:18 +0000

    Fix https para GoSocket

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if ( app()->environment('production') ) {
-            \Illuminate\Support\Facades\URL::forceScheme('http');
+            \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
 
