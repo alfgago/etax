730ab7765141d9a6ca051e6f0239635ddc16ae3b
Alfredo Gago <alfgago@gmail.com>
2020-03-13 22:36:53 +0000

    Fix IF de libro por correo

---
+++ app/Console/Commands/QueryPendingInvoices.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Invoice;
+use App\Jobs\ProcessInvoice;
+use App\Utils\BridgeHaciendaApi;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class QueryPendingInvoices extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'invoice:pending';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            $this->info('Sending invoices to Hacienda....');
+            $invoices = Invoice::where('hacienda_status', '05')
+                ->where('is_void', false)
+                ->where('resend_attempts', '<', 6)->where('in_queue', false)
+                ->whereIn('document_type', ['01', '03', '04', '08', '09'])
+                ->get();
+            $this->info('Querying pending invoices ....'. count($invoices));
+            $this->info('Get Token Api Hacienda ....');
+            $apiHacienda = new BridgeHaciendaApi();
+            $tokenApi = $apiHacienda->login(false);
+
+            foreach ($invoices as $invoice) {
+                try {
+        
+                    if ($tokenApi !== false) {
+                        $company = $invoice->company;
+                        $result = $apiHacienda->queryHacienda($invoice, $tokenApi, $company);
+                    }
+        
+                } catch (\Exception $e) {
+                    Log::error("Error consultado factura -->" .$e);
+                }
+            }
+        } catch ( \Exception $e) {
+            Log::error('Error in query pending command '.$e);
+        }
+    }
+}
