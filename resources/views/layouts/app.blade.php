52ee3853388878ef9d94ddd96d347034cbda54fc
Alfredo Gago <alfgago@gmail.com>
2020-07-08 16:59:29 +0000

    Fix cantidad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<?php $version = "6.50"; ?>
+<?php $version = "6.51"; ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
