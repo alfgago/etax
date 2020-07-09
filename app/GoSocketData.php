3f066e60514666a7aac191d329ed2135c7e28fcf
Alfredo Gago <alfgago@gmail.com>
2020-07-09 11:34:12 +0000

    Fix GSData

--- app/GoSocketData.php
+++ app/GoSocketData.php
@@ -4,14 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 
 class GoSocketData extends Model
 {
+
     use SoftDeletes;
+    protected $table = 'go_socket_data';
     protected $guarded = [];
     
 }
